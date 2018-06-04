6177125d5620621cc76d28ac6260c06107dcef7c
Jeffry Saputra <jeffrysaputra97@gmail.com>
2018-06-04 22:54:50 +0700

    Mengimplementasikan Start Service pada aplikasi
    Signed-off-by: Jeffry Saputra <jeffrysaputra97@yahoo.com>

--- app/src/main/java/com/ithb/jeffry/myservice/MainActivity.java
+++ app/src/main/java/com/ithb/jeffry/myservice/MainActivity.java
@@ -1,13 +1,36 @@
 package com.ithb.jeffry.myservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnStartService;
+    private Button btnStartIntentService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStartService = findViewById(R.id.btn_start_service);
+        btnStartService.setOnClickListener(this);
+        btnStartIntentService = findViewById(R.id.btn_start_intent_service);
+        btnStartIntentService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_start_service:
+                Intent mStartServiceIntent = new Intent(MainActivity.this, OriginService.class);
+                startService(mStartServiceIntent);
+                break;
+
+            case R.id.btn_start_intent_service:
+                break;
+        }
     }
 }
